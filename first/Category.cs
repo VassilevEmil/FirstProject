9fc3360207976e867c5bd7acdef661d732b15e7d
VassilevEmil <84077074+VassilevEmil@users.noreply.github.com>
2022-02-20 22:29:28 +0100

    Initial commit

---
+++ first/Category.cs
@@ -0,0 +1,9 @@
+﻿
+
+public enum Category
+{
+    Electronics,
+    Grocery,
+    Clothes
+
+}
---
+++ first/Program.cs
@@ -0,0 +1,30 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ShowPricesWithVat();
+    }
+
+    private static void ShowPricesWithVat()
+    {
+        Console.WriteLine("Product prices incl. VAT:");
+
+        foreach (var product in Products)
+        {
+            var vat = Utils.CalculateVat(product.Price, product.Category);
+            var priceWithVat = Math.Round(product.Price + vat, 2);
+            Console.WriteLine($"{product.Name}: {priceWithVat} EUR");
+        }
+    }
+
+    private static readonly IEnumerable<Product> Products = new List<Product>
+    {
+        new Product("Batteries", Category.Electronics, 2.50),
+        new Product("SD Card", Category.Electronics, 10),
+        new Product("T-shirt", Category.Electronics, 15),
+        new Product("Parmesan Cheese", Category.Grocery, 7.50),
+        new Product("Tomatoes", Category.Grocery, 2),
+    };
+
+}
